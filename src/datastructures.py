761017a8fc7bc0694cfc7ef11ae91a4869ded8cf
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2024-01-15 23:37:06 +0000

    all tested passed

--- src/datastructures.py
+++ src/datastructures.py
@@ -14,6 +14,28 @@ class FamilyStructure:
 
         # example list of members
         self._members = []
+        #adding the specified members
+        self.add_member({
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": self.last_name,
+            "age": 33,
+            "lucky_numbers": [7, 13, 22],
+        })
+        self.add_member({
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": self.last_name,
+            "age": 35,
+            "lucky_numbers": [10, 14, 3],
+        })
+        self.add_member({
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": self.last_name,
+            "age": 5,
+            "lucky_numbers": [2],
+        })
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,15 +43,25 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        #Check if 'id' is in member, if not, generate in id
+        if 'id' not in member:
+            member[id] = self._generateId()
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for i, member in enumerate(self._members):
+            if member['id'] == id:
+                return self._members.pop(i)
+        return "Member not found"
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id'] == id:
+                return member
+        return "Member not found"
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
